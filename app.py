8363735611cfe08ef5e1415e2fee3528cb95fc5a
Theodor <ampastheodwros@gmail.com>
2023-05-16 22:49:13 +0300

    EnotitaB peripou

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 # BEGIN CODE HERE
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
 from flask_cors import CORS
 from pymongo import TEXT
-# END CODE HERE
+import numpy as np
+from selenium import webdriver
+# BEGIN CODE HERE
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://127.0.0.1:27017/pspi"
@@ -11,30 +13,53 @@ CORS(app)
 mongo = PyMongo(app)
 mongo.db.products.create_index([("name", TEXT)])
 
-
 @app.route("/search", methods=["GET"])
 def search():
     # BEGIN CODE HERE
-    return ""
+    products = mongo.db.products.find()
+    result = []
+    for product in products:
+        result.append({
+            "id": str(product["_id"]),
+            "name": product["name"],
+            "production": product["production"],
+            "price": product["price"],
+            "color": product["color"],
+            "size": product["size"]
+        })
+
+    return jsonify(result)
     # END CODE HERE
 
 
 @app.route("/add-product", methods=["POST"])
 def add_product():
     # BEGIN CODE HERE
-    return ""
+    product = request.json
+    mongo.db.products.insert_one(product)
+    return jsonify({"message": "Product added successfully"})
     # END CODE HERE
 
 
 @app.route("/content-based-filtering", methods=["POST"])
 def content_based_filtering():
     # BEGIN CODE HERE
-    return ""
+    product_id = request.json["product_id"]
+    product = mongo.db.products.find_one({"_id": product_id})
+    if not product:
+        return jsonify({"error": "Product not found"})
+    similar_products = []
+    return jsonify(similar_products)
     # END CODE HERE
 
 
 @app.route("/crawler", methods=["GET"])
 def crawler():
     # BEGIN CODE HERE
-    return ""
+    driver = webdriver.Chrome()  
+    driver.quit()
+    return jsonify({"message": "Crawling completed"})
     # END CODE HERE
+
+if __name__ == "__main__":
+    app.run()
--- products.js
+++ products.js
@@ -1,19 +1,154 @@
 const api = "http://127.0.0.1:5000";
 
 window.onload = () => {
-    // BEGIN CODE HERE
+  // BEGIN CODE HERE
+  fetch(`${api}/search`)
+    .then(response => response.json())
+    .then(data => {
+      const table = document.getElementById("searchResultsTable");
+      table.innerHTML = `
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Production</th>
+          <th>Price</th>
+          <th>Color</th>
+          <th>Size</th>
+        </tr>
+      `;
 
-    // END CODE HERE
-}
+      data.forEach(product => {
+        table.innerHTML += `
+          <tr>
+            <td>${product.id}</td>
+            <td>${product.name}</td>
+            <td>${product.production}</td>
+            <td>${product.price}</td>
+            <td>${product.color}</td>
+            <td>${product.size}</td>
+          </tr>
+        `;
+      });
+
+      const searchResultsTitle = document.getElementById("searchResultsTitle");
+      searchResultsTitle.textContent = "Search Results:";
+    })
+    .catch(error => {
+      console.error("Error:", error);
+    });
+  // END CODE HERE
+};
 
 searchButtonOnClick = () => {
-    // BEGIN CODE HERE
+  // BEGIN CODE HERE
+  const searchInput = document.getElementById("searchInput").value;
 
-    // END CODE HERE
-}
+  fetch(`${api}/search?query=${searchInput}`)
+    .then(response => response.json())
+    .then(data => {
+      const table = document.getElementById("searchResultsTable");
+      table.innerHTML = `
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Production</th>
+          <th>Price</th>
+          <th>Color</th>
+          <th>Size</th>
+        </tr>
+      `;
+
+      data.forEach(product => {
+        table.innerHTML += `
+          <tr>
+            <td>${product.id}</td>
+            <td>${product.name}</td>
+            <td>${product.production}</td>
+            <td>${product.price}</td>
+            <td>${product.color}</td>
+            <td>${product.size}</td>
+          </tr>
+        `;
+      });
+
+      const searchResultsTitle = document.getElementById("searchResultsTitle");
+      searchResultsTitle.textContent = "Search Results:";
+    })
+    .catch(error => {
+      console.error("Error:", error);
+    });
+  // END CODE HERE
+};
 
 productFormOnSubmit = (event) => {
-    // BEGIN CODE HERE
+  // BEGIN CODE HERE
+  event.preventDefault();
 
-    // END CODE HERE
-}
+  const nameInput = document.getElementById("nameInput").value;
+  const productionInput = document.getElementById("productionInput").value;
+  const priceInput = document.getElementById("priceInput").value;
+  const colorInput = document.getElementById("colorInput").value;
+  const sizeInput = document.getElementById("sizeInput").value;
+
+  const product = {
+    name: nameInput,
+    production: productionInput,
+    price: priceInput,
+    color: colorInput,
+    size: sizeInput
+  };
+
+  fetch(`${api}/add-product`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(product)
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log("Success:", data);
+      alert("Product added successfully");
+      document.getElementById("addProductForm").reset();
+
+      fetch(`${api}/search`)
+        .then(response => response.json())
+        .then(data => {
+          const table = document.getElementById("searchResultsTable");
+          table.innerHTML = `
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Production</th>
+              <th>Price</th>
+              <th>Color</th>
+              <th>Size</th>
+            </tr>
+          `;
+
+          data.forEach(product => {
+            table.innerHTML += `
+              <tr>
+                <td>${product.id}</td>
+                <td>${product.name}</td>
+                <td>${product.production}</td>
+                <td>${product.price}</td>
+                <td>${product.color}</td>
+                <td>${product.size}</td>
+              </tr>
+            `;
+          });
+
+          const searchResultsTitle = document.getElementById("searchResultsTitle");
+          searchResultsTitle.textContent = "Search Results:";
+        })
+        .catch(error => {
+          console.error("Error:", error);
+        });
+    })
+    .catch(error => {
+      console.error("Error:", error);
+      alert("An error occurred while adding the product");
+    });
+  // END CODE HERE
+};
